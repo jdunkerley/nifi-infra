d2ca597a9f0c510f27efb3291467ed2d2ebca5f7
James Dunkerley <jdunkerley@scottlogic.com>
2018-10-03 22:13:15 +0100

    Add Powershell script Move to London

--- environments/dev/config.tf
+++ environments/dev/config.tf
@@ -1,6 +1,6 @@
 
 variable "region" {
-  default     = "eu-west-1"
+  default     = "eu-west-2"
   description = "The region to deploy the cluster in, e.g: us-east-1."
 }
 
@@ -14,9 +14,9 @@ variable "environment" {
 terraform {
   required_version = ">= 0.10.5"
   backend "s3" {
-    bucket = "nifi-terraform-eu-west-1"
+    bucket = "nifi-terraform-eu-west-2"
     key    = "dev/terraform.tfstate"
-    region = "eu-west-1"
+    region = "eu-west-2"
     encrypt = true
 
   }
@@ -25,9 +25,9 @@ terraform {
 data "terraform_remote_state" "default" {
   backend = "s3"
   config {
-    bucket = "nifi-terraform-eu-west-1"
+    bucket = "nifi-terraform-eu-west-2"
     key    = "dev/terraform.tfstate"
-    region = "eu-west-1"
+    region = "eu-west-2"
     encrypt = true
   }
 }
@@ -35,6 +35,6 @@ data "terraform_remote_state" "default" {
 
 # Define your AWS profile here
 provider "aws" {
-  region  = "eu-west-1"
+  region  = "eu-west-2"
   version = "~> 1.3.0"
 }
